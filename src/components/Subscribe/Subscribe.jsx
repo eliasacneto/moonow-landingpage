8d0c8c72e0f469f4cb873b44e7f4ebcf0f4a610d
netoeac <netoeac.dev@gmail.com>
2023-02-26 11:56:28 -0300

    field check validation implemented

--- src/components/Subscribe/Subscribe.jsx
+++ src/components/Subscribe/Subscribe.jsx
@@ -1,25 +1,69 @@
 import '../Subscribe/Subscribe.css'
 import Hero from '../../assets/images/hero-ilustration.png'
-export function Subscribe(){
-  return(
-  <main>
-    <section className='left-side'>
-      <h1 className='title'>Inscreva-se e viaje para a lua gratuitamente</h1>
+import { useState } from 'react';
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
 
-      <form>
-        <input className='input-form' type='text' id='fullname' name='fullname' placeholder='Nome Completo' required />
+export function Subscribe() {
 
-        <input className='input-form' type='email' id='email' name='email' placeholder='Email' />
+  const [fullName, setFullName] = useState('')
+  const [email, setEmail] = useState('')
+  const [msg, setMsg] = useState('')
+  const MySwal = withReactContent(Swal)
 
-      </form>
+  const formValidation = () => {
+    if (fullName === '') {
+      alert('Preencha o campo Nome!')
+      return false
+    }
+    if (email === '') {
+      alert('Preencha o campo Email!')
+      return false
+    }
 
-      <div className='button-group'>
-        <button className='go-moon'>Quero ir pra lua!</button>
-        <button className='more-info'>Mais informações</button>
-      </div>
-    </section>
-    <section className='right-side'>
-      <img src={Hero} alt='hero-illustration' />
-    </section>
-  </main>);
+    setMsg('')
+    // alert(msg)
+
+  }
+
+  return (
+    <>
+
+      <main>
+        <section className='left-side'>
+          <h1 className='title'>Inscreva-se e viaje para a lua gratuitamente</h1>
+
+          <form id='form'>
+
+            <input className='input-form' type='text' id='fullname' name='fullname' placeholder='Nome Completo' value={fullName} onChange={(e) => {
+              setFullName(e.target.value)
+            }
+            } />
+
+
+            <input className='input-form' type='email' id='email' name='email' placeholder='Email' value={email} onChange={(e) => {
+              setEmail(e.target.value)
+            }
+            } />
+
+
+            <div className='button-group'>
+              <button className='go-moon' onClick={formValidation}>Quero ir pra lua!</button>
+              <button className='more-info' id='more-info'>Mais informações</button>
+            </div>
+
+          </form>
+
+
+        </section>
+        <section className='right-side'>
+          <img className='image-hero' src={Hero} alt='hero-illustration' />
+        </section>
+      </main>
+
+
+    </>
+  );
 }
+
+// {msg}
